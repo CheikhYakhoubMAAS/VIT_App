28fc98be772aa567c9ccfdef24fa82193dc5fab7
CheikhYakhoubMAAS <72825635+CheikhYakhoubMAAS@users.noreply.github.com>
2023-05-28 22:24:52 +0000

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,201 @@
+import streamlit as st
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import torch.nn as nn
+import numpy as np
+import os
+import cv2
+
+
+def preprocess_input(image):
+    if isinstance(image, np.ndarray):
+        image = Image.fromarray(image)
+    transform = transforms.Compose([
+        transforms.Resize((28, 28)),
+        transforms.Grayscale(num_output_channels=1),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+    return transform(image).unsqueeze(0)
+
+def patchify(images, n_patches):
+    n, c, h, w = images.shape
+
+    assert h == w, "Patchify method is implemented for square images only"
+
+    patches = torch.zeros(n, n_patches ** 2, h * w * c // n_patches ** 2)
+    patch_size = h // n_patches
+
+    for idx, image in enumerate(images):
+        for i in range(n_patches):
+            for j in range(n_patches):
+                patch = image[:, i * patch_size: (i + 1) * patch_size, j * patch_size: (j + 1) * patch_size]
+                patches[idx, i * n_patches + j] = patch.flatten()
+    return patches
+
+
+class MyMSA(nn.Module):
+    def __init__(self, d, n_heads=2):
+        super(MyMSA, self).__init__()
+        self.d = d
+        self.n_heads = n_heads
+
+        assert d % n_heads == 0, f"Can't divide dimension {d} into {n_heads} heads"
+
+        d_head = int(d / n_heads)
+        self.q_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.k_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.v_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.d_head = d_head
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, sequences):
+        # Sequences has shape (N, seq_length, token_dim)
+        # We go into shape    (N, seq_length, n_heads, token_dim / n_heads)
+        # And come back to    (N, seq_length, item_dim)  (through concatenation)
+        result = []
+        for sequence in sequences:
+            seq_result = []
+            for head in range(self.n_heads):
+                q_mapping = self.q_mappings[head]
+                k_mapping = self.k_mappings[head]
+                v_mapping = self.v_mappings[head]
+
+                seq = sequence[:, head * self.d_head: (head + 1) * self.d_head]
+                q, k, v = q_mapping(seq), k_mapping(seq), v_mapping(seq)
+
+                attention = self.softmax(q @ k.T / (self.d_head ** 0.5))
+                seq_result.append(attention @ v)
+            result.append(torch.hstack(seq_result))
+        return torch.cat([torch.unsqueeze(r, dim=0) for r in result])
+
+
+class MyViTBlock(nn.Module):
+    def __init__(self, hidden_d, n_heads, mlp_ratio=4):
+        super(MyViTBlock, self).__init__()
+        self.hidden_d = hidden_d
+        self.n_heads = n_heads
+
+        self.norm1 = nn.LayerNorm(hidden_d)
+        self.mhsa = MyMSA(hidden_d, n_heads)
+        self.norm2 = nn.LayerNorm(hidden_d)
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_d, mlp_ratio * hidden_d),
+            nn.GELU(),
+            nn.Linear(mlp_ratio * hidden_d, hidden_d)
+        )
+
+    def forward(self, x):
+        out = x + self.mhsa(self.norm1(x))
+        out = out + self.mlp(self.norm2(out))
+        return out
+
+
+class MyViT(nn.Module):
+    def __init__(self, chw, n_patches=7, n_blocks=2, hidden_d=8, n_heads=2, out_d=10):
+        # Super constructor
+        super(MyViT, self).__init__()
+        
+        # Attributes
+        self.chw = chw # ( C , H , W )
+        self.n_patches = n_patches
+        self.n_blocks = n_blocks
+        self.n_heads = n_heads
+        self.hidden_d = hidden_d
+        
+        # Input and patches sizes
+        assert chw[1] % n_patches == 0, "Input shape not entirely divisible by number of patches"
+        assert chw[2] % n_patches == 0, "Input shape not entirely divisible by number of patches"
+        self.patch_size = (chw[1] / n_patches, chw[2] / n_patches)
+
+        # 1) Linear mapper
+        self.input_d = int(chw[0] * self.patch_size[0] * self.patch_size[1])
+        self.linear_mapper = nn.Linear(self.input_d, self.hidden_d)
+        
+        # 2) Learnable classification token
+        self.class_token = nn.Parameter(torch.rand(1, self.hidden_d))
+        
+        # 3) Positional embedding
+        self.register_buffer('positional_embeddings', get_positional_embeddings(n_patches ** 2 + 1, hidden_d), persistent=False)
+        
+        # 4) Transformer encoder blocks
+        self.blocks = nn.ModuleList([MyViTBlock(hidden_d, n_heads) for _ in range(n_blocks)])
+        
+        # 5) Classification MLPk
+        self.mlp = nn.Sequential(
+            nn.Linear(self.hidden_d, out_d),
+            nn.Softmax(dim=-1)
+        )
+
+    def forward(self, images):
+        # Dividing images into patches
+        n, c, _, h, w  = images.shape
+        #patches = patchify(images, self.n_patches).to(self.positional_embeddings.device)
+        patches = patchify(images.squeeze(0), self.n_patches).to(self.positional_embeddings.device)
+
+        # Running linear layer tokenization
+        # Map the vector corresponding to each patch to the hidden size dimension
+        tokens = self.linear_mapper(patches)
+        
+        # Adding classification token to the tokens
+        tokens = torch.cat((self.class_token.expand(n, 1, -1), tokens), dim=1)
+        
+        # Adding positional embedding
+        out = tokens + self.positional_embeddings.repeat(n, 1, 1)
+        
+        # Transformer Blocks
+        for block in self.blocks:
+            out = block(out)
+            
+        # Getting the classification token only
+        out = out[:, 0]
+        
+        return self.mlp(out) # Map to output dimension, output category distribution
+    
+
+
+def get_positional_embeddings(sequence_length, d):
+    result = torch.ones(sequence_length, d)
+    for i in range(sequence_length):
+        for j in range(d):
+            result[i][j] = np.sin(i / (10000 ** (j / d))) if j % 2 == 0 else np.cos(i / (10000 ** ((j - 1) / d)))
+    return result
+
+
+# Charger le modèle
+model = MyViT((1, 28, 28), n_patches=7, n_blocks=2, hidden_d=8, n_heads=2, out_d=10)
+model.load_state_dict(torch.load('model.pth', map_location=torch.device('cpu')),strict=False)
+model.to(torch.device('cpu'))
+
+# Liste des classes
+classes = list(range(10))
+# Interface Streamlit
+st.markdown('<h1 style="color:black;">VIT Image classification model</h1>', unsafe_allow_html=True)
+st.markdown('<h2 style="color:gray;">The image classification model classifies image into following categories:</h2>', unsafe_allow_html=True)
+st.markdown('<h3 style="color:gray;">0,1,2,3,4,5,6,7,8,9</h3>', unsafe_allow_html=True)
+
+upload = st.file_uploader('Insert image for classification', type=['png', 'jpg'])
+c1, c2 = st.columns(2)
+
+if upload is not None:
+    im = Image.open(upload)
+    img = np.asarray(im)
+    img = preprocess_input(img)
+    img = np.expand_dims(img, 0)
+    c1.header('Input Image')
+    c1.image(im)
+    c1.write(img.shape)
+
+    # Prédiction sur le modèle
+    with torch.no_grad():
+        output = model(torch.Tensor(img))
+    probabilities = torch.softmax(output, dim=1)
+    pred_class = torch.argmax(probabilities, dim=1)
+    pred_class = pred_class.item()
+    c2.header('Output')
+    c2.subheader('Predicted class:')
+    #c2.write(classes[pred_class])
+    c2.write(pred_class)
+    #c2.write(probabilities.tolist()[0])
+
